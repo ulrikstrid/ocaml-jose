1696bd77d17e9ccb4c7ddae5c767bda148bcb0c3
Ulrik <ulrik.strid@outlook.com>
2020-01-17 10:57:12 +0100

    Setup building with multiple versions in CI

---
+++ test-version.sh
@@ -0,0 +1,15 @@
+echo '{
+    "version": "1.0.0",
+    "name": "test",
+    "dependencies": {
+        "ocaml": "~'"$1"'",
+        "@opam/dune": "^2.0.0",
+        "@opam/jose": "*"
+    },
+    "resolutions": {
+        "@opam/nocrypto": "mirleft/ocaml-nocrypto:nocrypto.opam#80b7b4b9bd1ccfba3ec93d85cd82bfb3dc10f887",
+        "@opam/jose": "link:./package.json"
+    }
+}' > "$1.json"
+
+esy -P "$1"
