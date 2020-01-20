2593907a3879235c450e15b8a17ffb630666c284
Ulrik Strid <ulrik.strid@outlook.com>
2020-01-20 15:28:26 +0100

    test: Add opam to CI
    * Add opam to CI
    
    * Actually run the opam template
    
    * Fix opam download name
    
    * Move from correct folder
    
    * Set OPAMROOT in cache folder
    
    * Update opam file with test dependencies
    
    * Add test command in opam
    
    * Add build and update OPAMROOT
    
    * Disable build and test for now
    
    * Fix dependencies
    
    * Hopefully cache correctly
    
    * Move opam build to own stage
    
    * Add dune commands
    
    * Add zarith and yojson to opam file
    
    * Pin nocrypto
    
    * Set upper bound for dune
    
    * Correct test command
    
    * Remove test in opam for now

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,10 +1,9 @@
-rm -rf coverage
+rm -rf _coverage
 gsed -i '$i \(preprocess (pps bisect_ppx))\' jose/dune
 
 BISECT_ENABLE=yes esy test
-esy bisect-ppx-report -html ./coverage bisect0001.out
+esy bisect-ppx-report html
+esy bisect-ppx-report summary
 
 git checkout jose/dune
-rm bisect0001.out
-
-open coverage/index.html
+rm bisect*
