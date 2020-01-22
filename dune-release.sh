7416d07dba98efbd4711b8fa67776f892890fefb
Ulrik Strid <ulrik.strid@outlook.com>
2020-01-22 21:32:50 +0100

    chore: Prepare release 0.1.0
    * Remove commented out test in opam CI
    
    * Set description in dune-project and jose.opam
    
    * Fix ignore files
    
    * Add CHANGES
    
    * Update readme
    
    * Update CHANGES
    
    * Rename CHANGES to CHANGES.md
    
    * Add script to do releases, borrowed from merlin

---
+++ dune-release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+TAG="$1"
+
+if [ -z "$TAG" ]; then
+  printf "Usage: ./dune-release.sh <tag-name>\n"
+  printf "Please make sure that dune-release is available.\n"
+  exit 1
+fi
+
+step()
+{
+  printf "Continue? [Yn] "
+  read action
+  if [ "x$action" == "xn" ]; then exit 2; fi
+  if [ "x$action" == "xN" ]; then exit 2; fi
+}
+
+dune-release tag "$TAG"
+step
+dune-release distrib -p jose -n jose -t "$TAG" --skip-tests #--skip-lint
+step
+dune-release publish distrib -p jose -n jose -t "$TAG"
+step
+dune-release opam pkg -p jose -n jose -t "$TAG"
+step
+dune-release opam submit -p jose -n jose -t "$TAG"
