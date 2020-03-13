0c11375d588c91d2bdc3b04827dc138979df2eef
Ulrik Strid <ulrik.strid@outlook.com>
2020-03-13 10:58:58 +0100

    Update test-version.sh

--- test-version.sh
+++ test-version.sh
@@ -3,11 +3,10 @@ echo '{
     "name": "test",
     "dependencies": {
         "ocaml": "~'"$1"'",
-        "@opam/dune": "1.11.0",
+        "@opam/dune": ">=1.11.0",
         "@opam/jose": "*"
     },
     "resolutions": {
-        "@opam/nocrypto": "mirleft/ocaml-nocrypto:nocrypto.opam#80b7b4b9bd1ccfba3ec93d85cd82bfb3dc10f887",
         "@opam/jose": "link:./package.json"
     }
 }' > "$1.json"
