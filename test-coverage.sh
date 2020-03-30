d02b784639960cfa3d2a204d16ae30d881bdf0eb
Ulrik <ulrik.strid@outlook.com>
2020-03-30 09:14:07 +0200

    Simplify coverage script

--- test-coverage.sh
+++ test-coverage.sh
@@ -2,24 +2,11 @@ echo removing _coverage dir
 rm -rf _coverage
 rm junit.xml
 
-echo patching dune file
-gsed -i '$i \(preprocess (pps bisect_ppx))\' jose/dune
-
 echo running tests
 BISECT_ENABLE=yes REPORT_PATH=./junit.xml esy test --force
 
-esy echo "#{self.target_dir / 'default' / 'test'}"
-
 cp $(esy echo "#{self.target_dir / 'default' / 'test' / 'junit.xml'}") ./junit.xml
-
-echo copying bisect files
-cp $(esy echo "#{self.target_dir / 'default' / 'test'}")/[bisect]* ./
 
 echo generating reports
 esy bisect-ppx-report html
 esy bisect-ppx-report summary
-
-echo reseting files
-git checkout jose/dune
-rm bisect*
-rm test.{exe,ml}
