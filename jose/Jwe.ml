c8796efe70abf6bf7e52178d7d7055402fb38e92
Ulrik <ulrik.strid@outlook.com>
2020-04-11 21:43:31 +0200

    fix formatting

--- jose/Jwe.ml
+++ jose/Jwe.ml
@@ -1,18 +1,11 @@
 (** {{: https://tools.ietf.org/html/rfc7516 } Link to RFC } *)
 
-type aad
 (** Additional Authentication Data *)
+type aad
 
-type protected
 (** JWE Protected Header *)
+type protected
 
-type t = {
-  header : Header.t;
-  cek : string;
-  init_vector : string;
-  payload : string;
-  aad : aad option;
-}
 (**
 JOSE header
 .
@@ -26,6 +19,13 @@ JWE Ciphertext
 .
 JWE Authentication Tag
  *)
+type t = {
+  header : Header.t;
+  cek : string;
+  init_vector : string;
+  payload : string;
+  aad : aad option;
+}
 
 (*
 Steps to create a JWE
@@ -82,12 +82,12 @@ let decrypt_cek alg str ~(jwk : Jwk.priv Jwk.t) =
     | Some c -> Ok (Cstruct.to_string c)
     | None -> Error `Decrypt_cek_failed
   in
-  match alg, jwk with
+  match (alg, jwk) with
   | Jwa.RSA1_5, Jwk.Rsa_priv rsa ->
-    Utils.RBase64.url_decode str
-    |> RResult.map Cstruct.of_string
-    |> RResult.map (Mirage_crypto_pk.Rsa.PKCS1.decrypt ~key:rsa.key)
-    |> RResult.flat_map of_opt_cstruct
+      Utils.RBase64.url_decode str
+      |> RResult.map Cstruct.of_string
+      |> RResult.map (Mirage_crypto_pk.Rsa.PKCS1.decrypt ~key:rsa.key)
+      |> RResult.flat_map of_opt_cstruct
   | _ -> Error `Invalid_JWK
 
 let pkcs7_unpad cs =
@@ -95,53 +95,54 @@ let pkcs7_unpad cs =
   let pad_len = Cstruct.get_uint8 cs (cs_len - 1) in
   let data, padding = Cstruct.split cs (cs_len - pad_len) in
   let rec check idx =
-    if idx >= pad_len then true else
-      (Cstruct.get_uint8 padding idx = pad_len) && check (idx + 1)
+    if idx >= pad_len then true
+    else Cstruct.get_uint8 padding idx = pad_len && check (idx + 1)
   in
   if check 0 then Ok data else Error (`Msg "bad padding")
 
 let bind v f = match v with Ok v -> f v | Error _ as e -> e
 
-let (>>=) = bind
+let ( >>= ) = bind
 
 let decrypt_ciphertext enc ~cek ~init_vector ~auth_tag ~aad ciphertext =
   match enc with
   | Some Jwa.A128CBC_HS256 ->
-    (* RFC 7516 appendix B.1: first 128 bit hmac, last 128 bit aes *)
-    let hmac_key, aes_key = Cstruct.(split (of_string cek) 16) in
-    let key = Mirage_crypto.Cipher_block.AES.CBC.of_secret aes_key in
-    let iv = Cstruct.of_string init_vector in
-    RBase64.url_decode ciphertext >>= fun encrypted ->
-    let encrypted = Cstruct.of_string encrypted in
-    (* B.5 input to HMAC computation *)
-    let hmac_input =
-      (* B.3 64 bit big-endian AAD length (in bits!) *)
-      let aal = Cstruct.create 8 in
-      Cstruct.BE.set_uint64 aal 0 Int64.(mul 8L (of_int (String.length aad)));
-      Cstruct.(concat [ of_string aad ; iv ; encrypted ; aal ])
-    in
-    let computed_auth_tag =
-      let full = Mirage_crypto.Hash.SHA256.hmac ~key:hmac_key hmac_input in
-      (* B.7 truncate to 128 bit *)
-      Cstruct.sub full 0 16 |> Cstruct.to_string
-    in
-    if not (String.equal computed_auth_tag auth_tag) then
-      Error (`Msg "invalid auth tag")
-    else
-      (* B.2 encryption in CBC mode *)
-      Mirage_crypto.Cipher_block.AES.CBC.decrypt ~key ~iv encrypted
-      |> pkcs7_unpad >>= fun data ->
-      Ok (Cstruct.to_string data)
+      (* RFC 7516 appendix B.1: first 128 bit hmac, last 128 bit aes *)
+      let hmac_key, aes_key = Cstruct.(split (of_string cek) 16) in
+      let key = Mirage_crypto.Cipher_block.AES.CBC.of_secret aes_key in
+      let iv = Cstruct.of_string init_vector in
+      RBase64.url_decode ciphertext >>= fun encrypted ->
+      let encrypted = Cstruct.of_string encrypted in
+      (* B.5 input to HMAC computation *)
+      let hmac_input =
+        (* B.3 64 bit big-endian AAD length (in bits!) *)
+        let aal = Cstruct.create 8 in
+        Cstruct.BE.set_uint64 aal 0 Int64.(mul 8L (of_int (String.length aad)));
+        Cstruct.(concat [ of_string aad; iv; encrypted; aal ])
+      in
+      let computed_auth_tag =
+        let full = Mirage_crypto.Hash.SHA256.hmac ~key:hmac_key hmac_input in
+        (* B.7 truncate to 128 bit *)
+        Cstruct.sub full 0 16 |> Cstruct.to_string
+      in
+      if not (String.equal computed_auth_tag auth_tag) then
+        Error (`Msg "invalid auth tag")
+      else
+        (* B.2 encryption in CBC mode *)
+        Mirage_crypto.Cipher_block.AES.CBC.decrypt ~key ~iv encrypted
+        |> pkcs7_unpad
+        >>= fun data -> Ok (Cstruct.to_string data)
   | _ -> Error (`Msg "unsupported encryption")
-
 
 let decrypt jwe ~(jwk : Jwk.priv Jwk.t) =
   String.split_on_char '.' jwe |> function
-  | [ enc_header; enc_cek; enc_init_vector; ciphertext; auth_tag ] -> (
+  | [ enc_header; enc_cek; enc_init_vector; ciphertext; auth_tag ] ->
       Header.of_string enc_header >>= fun header ->
       decrypt_cek header.Header.alg ~jwk enc_cek >>= fun cek ->
       RBase64.url_decode enc_init_vector >>= fun init_vector ->
       RBase64.url_decode auth_tag >>= fun auth_tag ->
-      decrypt_ciphertext header.Header.enc ~cek ~init_vector ~auth_tag ~aad:enc_header ciphertext >>= fun payload ->
-      Ok { header; cek; init_vector = enc_init_vector; payload; aad = None })
+      decrypt_ciphertext header.Header.enc ~cek ~init_vector ~auth_tag
+        ~aad:enc_header ciphertext
+      >>= fun payload ->
+      Ok { header; cek; init_vector = enc_init_vector; payload; aad = None }
   | _ -> Error `Invalid_JWE
