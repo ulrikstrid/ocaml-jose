c5287f2891f20b584ec5ed0b3d0b0b37f1f194d7
Ulrik <ulrik.strid@outlook.com>
2020-04-13 10:33:08 +0200

    Update github actions

---
+++ .github/workflows/update_package_version.js
@@ -0,0 +1,24 @@
+const Fs = require("fs");
+const Path = require("path");
+
+const version = Fs.readFileSync(Path.resolve("VERSION"), { encoding: "utf8" })
+  .toString()
+  .trim();
+
+const updateVersion = (file) => {
+  console.log(`Updating file ${file}`);
+  const packageJson = Fs.readFileSync(file, { encoding: "utf8" });
+  const updatedPackageJson = JSON.parse(packageJson);
+  updatedPackageJson.version = version;
+
+  Fs.writeFileSync(file, JSON.stringify(updatedPackageJson, null, 2));
+};
+
+Fs.readdirSync(".", { encoding: "utf8" })
+  .filter(
+    (file) =>
+      file.includes(".json") &&
+      !file.includes("esy") &&
+      !file.includes("benchmark")
+  )
+  .map(updateVersion);
