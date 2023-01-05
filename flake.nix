e9a2c2152c433ae30512ea26b552f3f3ddaf811d
Ulrik Strid <ulrik.strid@outlook.com>
2023-01-05 10:15:33 +0100

    Update flakes

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
-  description = "my project description";
+  description = "JOSE implementation in OCaml";
+
+  nixConfig = {
+    extra-substituters = "https://anmonteiro.nix-cache.workers.dev";
+    extra-trusted-public-keys = "ocaml.nix-cache.com-1:/xI2h2+56rwFfKyyFVbkJSeGqSIYMC/Je+7XXqGKDIY=";
+  };
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.inputs.flake-utils.follows = "flake-utils";
-    nixpkgs.url = "github:anmonteiro/nix-overlays";
+    nixpkgs.url = "github:nix-ocaml/nix-overlays";
 
     nix-filter.url = "github:numtide/nix-filter";
   };
