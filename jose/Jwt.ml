36356e00571361f0dd0153fe640b29cf2604bce3
Ulrik Strid <ulrik.strid@outlook.com>
2023-02-19 14:31:24 +0100

    Cleanup after Ptime PR

--- jose/Jwt.ml
+++ jose/Jwt.ml
@@ -81,9 +81,7 @@ let check_expiration ~(now : Ptime.t) t =
       in
 
       if is_earlier = Some true then Ok t else Error `Expired
-  | None ->
-      print_endline "no exp";
-      Ok t
+  | None -> Ok t
 
 let validate_signature (type a) ~(jwk : a Jwk.t) (t : t) : (t, 'error) result =
   Jws.validate ~jwk (to_jws t) |> U_Result.map of_jws
