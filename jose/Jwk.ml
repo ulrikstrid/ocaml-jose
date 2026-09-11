e26aa292bc54f099fbb0af8a566dcd8128a6c817
Ulrik Strid <ulrik@strid.tech>
2026-09-11 13:05:40 +0200

    Add Direct Encryption "dir" alg

--- jose/Jwk.ml
+++ jose/Jwk.ml
@@ -76,7 +76,7 @@ let alg_of_use_and_kty ?(use : use = `Sig) (kty : Jwa.kty) : Jwa.alg =
   | `Sig, `EC -> `ES512
   | `Sig, `OKP -> `Ed25519
   | `Enc, `RSA -> `RSA_OAEP
-  | `Enc, `oct -> `Unsupported "encryption with oct is not supported yet"
+  | `Enc, `oct -> `Dir
   | `Enc, `EC ->
       `Unsupported "encryption with eliptic curves are not supported yet"
   | `Enc, `OKP ->
@@ -94,6 +94,7 @@ let use_of_alg (alg : Jwa.alg) =
   | `EdDSA | `Ed25519 -> `Sig
   | `RSA_OAEP -> `Enc
   | `RSA1_5 -> `Enc
+  | `Dir -> `Enc
   | `None -> `Unsupported "none"
   | `Unsupported str -> `Unsupported str
 
